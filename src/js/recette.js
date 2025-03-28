cf2211300ab05b7b7b072550c944fed383053dce
moralesjustin19 <justin.mogrovejo@laplateforme.io>
2025-03-28 10:23:48 +0100

    ✨Mise en place de modal afin de montrer les infos de recettes, modification data.json pour ajout des id, mise en place de boutons (+) dans la modale pour ajouter des ingredients a la liste de courses et ajout de lignes css pour placement des boutons par rapport aux ingrédients.

--- src/js/recette.js
+++ src/js/recette.js
@@ -21,14 +21,40 @@ $(document).ready(function () {
                 $(containerId).empty(); // Vider le conteneur avant d'ajouter de nouvelles recettes
 
                 recettesPage.forEach(recette => {
+                    console.log(recette);
+                    
                     let recetteHTML = `
                         <div class="col-md-4">
                             <div class="card bg-secondary">
                                 <img src="${recette.img}" class="card-img-top" alt="${recette.nom}">
                                 <div class="card-body text-center">
                                     <h2 class="card-title fs-5">${recette.nom}</h2>
-                                    <button class="btn btn-dark">Détails</button>
+                                    <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#detailsRecette-${recette.id}">Détails</button>
                                     <button class="favorite-btn">⭐</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="modal fade" id="detailsRecette-${recette.id}" tabindex="-1" aria-labelledby="detailsRecetteLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5 text-dark" id="detailsRecetteLabel">${recette.nom}</h1>
+                                        <p class="fs-6 text-muted">${recette.categorie}</p>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h2 class="fs-6 text-dark">Liste d'ingrédients</h2>
+                                            <ul class="fs-6 text-dark">${recette.ingredients.map(ingredient => `
+                                                <li class="ingredient-item"><span>${ingredient.nom}</span><button type="button" class="btn btn-primary">+</button>
+                                                </li>`).join('')}
+                                            </ul>
+
+                                        <h3 class="fs-6 text-dark">${recette.etapes}</h3>
+                                        <p class="fs-6 text-dark">Durée :${recette.temps_preparation}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fermer</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
